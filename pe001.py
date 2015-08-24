b5d2059dc12f813c3c29118ef5eb422f36f23e0d
Adam Dinwoodie <adam@dinwoodie.org>
2015-08-24 11:17:08 +0100

    Switch problem 1 to a one-line solution
    Mostly because Hark on #maelfroth suggested it, and I realised there was
    no need for a full generator function when a generator expression would
    do the job just as well.

--- pe001.py
+++ pe001.py
@@ -8,14 +8,8 @@ get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 '''
 
-from itertools import takewhile
-
-def multiple_generator():
-    num = 1
-    while True:
-        if (num % 3) == 0 or (num % 5) == 0:
-            yield num
-        num += 1
+from itertools import takewhile, count
 
 if __name__ == '__main__':
-    print(sum(takewhile(lambda x: x < 1000, multiple_generator())))
+    print(sum(takewhile(lambda x: x < 1000,
+                        (n for n in count() if n % 3 == 0 or n % 5 == 0))))
