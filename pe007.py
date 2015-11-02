23f5e8000bc0ae2dc25f21e963297bfcff6d16f8
Adam Dinwoodie <Adam.Dinwoodie@metaswitch.com>
2015-11-02 18:47:45 +0000

    Solution for problem 7
    More-or-less trivial thanks to the functions written for previous prime
    generation.

---
+++ pe007.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''
+10001st prime
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11 and 13, we can see that
+the 6th prime is 13.
+
+What is the 10001st prime number?
+'''
+
+from sys import argv
+from itertools import islice
+
+from prime import prime_generator
+
+if __name__ == '__main__':
+    try:
+        target = int(argv[1])
+    except IndexError:
+        target = 10001
+
+    # Use islice to discard the first `target - 1` primes.
+    print(next(islice(prime_generator(), target - 1, None)))
