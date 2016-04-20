355804393afecd5556a80d723e501ee5d0af61c4
Adam Dinwoodie <adam@dinwoodie.org>
2016-04-20 16:06:17 +0100

    Add function for generating divisors in order

--- divisors.py
+++ divisors.py
@@ -16,3 +16,16 @@ def divisors(x):
             yield i
             if i != 1 and i * i != x:
                 yield x // i
+
+
+def ordered_divisors(x):
+    '''Generate the proper divisors of a number, in order.'''
+    # Based broadly on http://stackoverflow.com/a/171779/220155
+    not_yet_yielded = []
+    for i in range(1, int(sqrt(x) + 1)):
+        if x % i == 0:
+            yield i
+            if i != 1 and i * i != x:
+                not_yet_yielded.insert(0, x // i)
+    for i in not_yet_yielded:
+        yield i
