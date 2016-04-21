077b7e1ea2243b75f3dee8e91a585df2243910f5
Adam Dinwoodie <adam@dinwoodie.org>
2016-04-21 11:42:41 +0100

    Solution for problem 25

--- pe025.py
+++ pe025.py
@@ -26,3 +26,20 @@ The 12th term, F12, is the first term to contain three digits.
 What is the index of the first term in the Fibonacci sequence to contain 1000
 digits?
 '''
+from sys import argv
+
+from fibonacci import fibonacci
+
+
+if __name__ == '__main__':
+    try:
+        digits = int(argv[1])
+    except IndexError:
+        digits = 1000
+
+    goal = 10 ** (digits - 1)
+
+    for i, f in enumerate(fibonacci(), 1):
+        if f >= goal:
+            print(i)
+            break
