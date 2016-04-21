3ee154b4fbc4f1975de0042430799e084aa30d62
Adam Dinwoodie <adam@dinwoodie.org>
2016-04-21 11:23:27 +0100

    Solution for problem 24

--- pe024.py
+++ pe024.py
@@ -12,3 +12,17 @@ lexicographic permutations of 0, 1 and 2 are:
 What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5,
 6, 7, 8 and 9?
 '''
+
+from sys import argv
+from itertools import islice, permutations
+
+if __name__ == '__main__':
+    try:
+        target = int(argv[1])
+    except IndexError:
+        target = 1000000
+
+    # Use islice to discard the first `target - 1` permutations.
+    print(''.join(next(islice(permutations('0123456789', 10),
+                              target - 1,
+                              None))))
