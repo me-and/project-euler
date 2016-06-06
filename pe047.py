eaa28f51ecb6e47aad6587b403f7108cef2713b6
Adam Dinwoodie <adam@dinwoodie.org>
2016-06-06 13:35:34 +0100

    Solution for problem 47

--- pe047.py
+++ pe047.py
@@ -16,3 +16,18 @@ The first three consecutive numbers to have three distinct prime factors are:
 Find the first four consecutive integers to have four distinct prime factors.
 What is the first of these numbers?
 '''
+
+from itertools import count
+
+from prime import prime_factors
+
+if __name__ == '__main__':
+    sequence = 0
+    for number in count(1):
+        if len(prime_factors(number)) == 4:
+            sequence += 1
+            if sequence == 4:
+                print(number - 3)
+                break
+        else:
+            sequence = 0
