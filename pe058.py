475f360402d1b20319879887786dc36a1d503f28
Adam Dinwoodie <adam@dinwoodie.org>
2018-01-30 15:51:48 +0000

    pe058: use itertools.count
    Rather than incrementing things in a loop manually, just use
    itertools.count to increment automatically each run through the loop.

--- pe058.py
+++ pe058.py
@@ -23,31 +23,26 @@ side length of the square spiral for which the ratio of primes along both
 diagonals first falls below 10%?
 '''
 
+from itertools import count
+
 from prime import MillerRabinPrimes
 
 
 def generate_corners():
-    increment = 10
-    side_length = 2
     value = 3
-    while True:
+    for increment, side_length in zip(count(10, 8), count(2, 2)):
         # Don't yield the fourth corner, as it's always going to be a perfect
         # square and thus never prime.
         yield (value, value + side_length, value + side_length * 2)
         value += increment
-        side_length += 2
-        increment += 8
+
 
 if __name__ == '__main__':
-    num_corners = 1
-    num_primes = 0
-    side_length = 1
-
     primes = MillerRabinPrimes()
+    num_primes = 0
 
-    for corners in generate_corners():
-        num_corners += 4
-        side_length += 2
+    for corners, num_corners, side_length in zip(generate_corners(),
+                                                 count(1, 4), count(1, 2)):
         num_primes += sum(corner in primes for corner in corners)
         if num_primes * 10 < num_corners:
             print(side_length)
