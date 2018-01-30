c5f8269ffcde29728eef0c1cbf8f575a944d37cb
Adam Dinwoodie <adam@dinwoodie.org>
2018-01-30 17:55:17 +0000

    wip

--- polygons.py
+++ polygons.py
@@ -8,3 +8,5 @@ pentagons = MonatonicIncreasingSequence(n * (3 * n - 1) // 2 for n in count(1))
 hexagons = MonatonicIncreasingSequence(n * (2 * n - 1) for n in count(1))
 heptagons = MonatonicIncreasingSequence(n * (5 * n - 3) // 2 for n in count(1))
 octagons = MonatonicIncreasingSequence(n * (3 * n - 2) for n in count(1))
+
+cubes = MonatonicIncreasingSequence(i ** 3 for i in count(1))
